64f37575c61308eeac9042ea78ba0dc82f83fb28
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2022-05-02 10:48:41 -0300

    import

--- src/Controller/useCases/accountUseCases/UpdateNameAccount.java
+++ src/Controller/useCases/accountUseCases/UpdateNameAccount.java
@@ -1,6 +1,5 @@
 package Controller.useCases.accountUseCases;
 
-import Model.Account;
 import java.util.Objects;
 
 import Controller.Database.Database;
@@ -8,7 +7,7 @@ import Controller.Database.Database;
 public class UpdateNameAccount {
     public static void execute(String name, String login, Database database){
         if(!database.isEmpty()){
-            for(int i = 0; i < database.accounts.length; i++) {
+            for(int i = 0; i < database.length(); i++) {
                 if(Objects.equals(database.accounts[i].getLogin(), login)){
                     database.accounts[i].setName(name);
                     System.out.println("Nome alterado com sucesso!");
